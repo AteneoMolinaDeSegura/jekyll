7395bb09728eb3f9549b36b7239c4bbe7b2891c6
Pedro Vicente Gómez Sánchez <pedrovicente.gomez@gmail.com>
2018-09-11 00:39:07 +0200

    Optimize images during the build execution in production

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -2,3 +2,13 @@
 set -e # halt script on error
 
 JEKYLL_ENV=production bundle exec jekyll build
+if [ $? -eq 0 ]; then
+  cd _site/images
+  mv posts original_posts_images
+  mkdir posts
+  cd original_posts_images
+  find '.' -name '*.png' -exec pngquant --quality=65-80 --output ../posts/{}.png {} \;
+  find '.' -name '*.jpg' -exec jpegoptim --strip-all --all-progressive -f --dest=../posts/ {} \;
+  find '.' -name '*.jpeg' -exec jpegoptim --strip-all --all-progressive -f --dest=../posts/ {} \;
+  rm -rf ../original_posts_images
+fi
