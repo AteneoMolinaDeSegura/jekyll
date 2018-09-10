d890ebb5b337fd1a5ebd51e205a5f278b0d7daf6
Pedro Vicente Gómez Sánchez <pedrovicente.gomez@gmail.com>
2018-09-11 01:10:39 +0200

    Prevent the jpeg file compression to fail

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -2,14 +2,23 @@
 set -e # halt script on error
 
 JEKYLL_ENV=production bundle exec jekyll build
+
+compressJpgFile() {
+  jpegoptim --max=80 --strip-all --all-progressive -f --dest=../posts/ $1
+  if [ $? -ne 0 ]; then
+    cp $1 ../posts
+  fi
+}
+export -f compressJpgFile
+
 if [ $? -eq 0 ]; then
   cd _site/images
   mv posts original_posts_images
   mkdir posts
   cd original_posts_images
   find '.' -name '*.png' -exec pngquant --quality=65-80 --output ../posts/{} {} \;
-  find '.' -name '*.jpg' -exec jpegoptim --strip-all --all-progressive -f --dest=../posts/ {} \;
-  find '.' -name '*.jpeg' -exec jpegoptim --strip-all --all-progressive -f --dest=../posts/ {} \;
+  find '.' -name '*.jpg' -exec bash -c 'compressJpgFile "{}"' \;
+  find '.' -name '*.jpeg' -exec bash -c 'compressJpgFile "{}"' \;
   cd ..
 
   echo "Original posts images size:"
